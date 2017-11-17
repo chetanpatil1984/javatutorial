82c7a5479bd7bd3adefc11d23997071c3241367c
Chetan Subhash Patil <chetan.subhash.patil@gmail.com>
2017-11-17 17:02:58 +0530

    ThreadGroup

--- javaproject/src/j2se/code/Snippet1ThreadGroup.java
+++ javaproject/src/j2se/code/Snippet1ThreadGroup.java
@@ -1,33 +1,36 @@
 /*
- * Class: Main Created on Nov 16, 2017 (c) Chetan Subhash Patil, unpublished
- * work, All use, disclosure, and/or reproduction of this material is prohibited
- * unless authorized in writing. All Rights Reserved. Rights in this program
- * belong to: Chetan Subhash Patil
+ * Class: Scrapbook1
+ * 
+ * Created on Nov 16, 2017
+ * 
+ * (c) Chetan Subhash Patil, unpublished work,
+ * All use, disclosure, and/or reproduction of this material is prohibited
+ * unless authorized in writing.  All Rights Reserved.
+ * Rights in this program belong to:
+ * Chetan Subhash Patil
  */
-package j2se;
+package j2se.code;
 
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.Callable;
 
-import j2se.code.Snippet1ThreadGroup;
-
 /**
  * @author chetpati
+ *
  */
-public class Main extends Thread
+public class Snippet1ThreadGroup extends Thread
 {
-  
   private static ThreadGroup tg1 = new ThreadGroup("threadgroup");
   private static ThreadGroup tg2 = new ThreadGroup("threadgroup");
   
-  public Main(String string)
+  public Snippet1ThreadGroup(String string)
   {
     super(string);
   }
   
-  public Main(ThreadGroup tg, String string)
+  public Snippet1ThreadGroup(ThreadGroup tg, String string)
   {
     super(tg,string);
   }
@@ -51,19 +54,6 @@ public class Main extends Thread
     }
   }
   
-  
 }
-
-
-class Task implements Callable<List<String>>
-{
-  
-  public List<String> call() throws IOException
-  {
-    return Arrays.asList("Apple","Oranges");
-  }
-  
-}
-
 
 
