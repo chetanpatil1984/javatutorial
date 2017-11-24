ab88a5bad0988aaf0b16532b571f074521b04198
Chetan Subhash Patil <chetan.subhash.patil@gmail.com>
2017-11-25 03:10:24 +0530

    exclude bin

--- shellproject/utilities/utility.sh
+++ shellproject/utilities/utility.sh
@@ -2,4 +2,4 @@
 
 #rsync -hvuzr /scratch/chetpati/view_storage/chetpati_bip_main_raj/xdo /scratch/chetpati/chetan/work/rsync/chetpati_bip_main/
 rsync -avz chetpati@adc00raj.us.oracle.com:/scratch/chetpati/chetan/work/rsync /cygdrive/d/chetan/work/
-rsync -avz /cygdrive/d/chetan/work/rsync/chetpati_bip_main/xdo /cygdrive/d/chetan/work/bipublisher/chetpati_bip_main_backup/
+rsync -avz --exclude bin "/cygdrive/d/chetan/work/rsync/chetpati_bip_main/xdo" "/cygdrive/d/chetan/work/bipublisher/chetpati_bip_main_backup/"
