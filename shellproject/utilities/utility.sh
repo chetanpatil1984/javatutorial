62ec835f65eb16cd413a5e1261b0b1bb5098e4f0
Chetan Subhash Patil <chetan.subhash.patil@gmail.com>
2017-11-25 02:23:38 +0530

    minor correction

--- shellproject/utilities/utility.sh
+++ shellproject/utilities/utility.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 #rsync -hvuzr /scratch/chetpati/view_storage/chetpati_bip_main_raj/xdo /scratch/chetpati/chetan/work/rsync/chetpati_bip_main/
-rsync -avz chetpati@adc00raj.us.oracle.com:/scratch/chetpati/chetan/work/rsync /cygdrive/d/chetan/work/rsync
+rsync -avz chetpati@adc00raj.us.oracle.com:/scratch/chetpati/chetan/work/rsync /cygdrive/d/chetan/work/
