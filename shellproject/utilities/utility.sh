b347d7484c753e345ebfc7ebfdeac585e496c77b
Chetan Subhash Patil <chetan.subhash.patil@gmail.com>
2017-11-27 20:24:39 +0530

    robocopy

--- shellproject/utilities/utility.sh
+++ shellproject/utilities/utility.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
-#rsync -hvuzr /scratch/chetpati/view_storage/chetpati_bip_main_raj/xdo /scratch/chetpati/chetan/work/rsync/chetpati_bip_main/
-rsync -avz chetpati@adc00raj.us.oracle.com:/scratch/chetpati/chetan/work/rsync /cygdrive/d/chetan/work/
-rsync -avz --exclude bin "/cygdrive/d/chetan/work/rsync/chetpati_bip_main/xdo" "/cygdrive/d/chetan/work/bipublisher/chetpati_bip_main_backup/"
+#rsync -hvuzr /scratch/chetpati/view_storage/chetpati_bip_main_raj/xdo /scratch/chetpati/chetan/work/rsync/chetpati_bip_main
+rsync -avz --no-l --exclude "*.class" chetpati@adc00raj.us.oracle.com:/scratch/chetpati/chetan/work/rsync /cygdrive/d/chetan/work
+rsync -avz --no-l --exclude "*.class" "/cygdrive/d/chetan/work/rsync/chetpati_bip_main/xdo" "/cygdrive/d/chetan/work/bipublisher/chetpati_bip_main_backup"
+
+robocopy d:/chetan/work/rsync/chetpati_bip_main d:/chetan/work/bipublisher/chetpati_bip_main /E /MT:64
