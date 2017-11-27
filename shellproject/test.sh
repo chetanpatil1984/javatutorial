615aff686a22fe86a1d83fa474493f32838884d8
Chetan Subhash Patil <chetan.subhash.patil@gmail.com>
2017-11-27 21:35:39 +0530

    test

--- shellproject/test.sh
+++ shellproject/test.sh
@@ -6,7 +6,7 @@ while true
 do
 	STATUS_CODE=`curl --silent -w "%{http_code}" --output /dev/null --header "Content-Type: text/xml;cset=UTF-8" --header "SOAPAction:http://xmlns.oracle.com/oxp/service/v2:getReportDefinition" --data @request.xml http://slc11cfs.us.oracle.com:9704/xmlpserver/services/v2/ReportService`
 
-	if [ "${STATUS_CODE}" -eq 200 ]
+	if [ "${STATUS_CODE}" -eq 200 ] 
 	  then
 		echo "ping bip ${PING_COUNT} status ${STATUS_CODE}"
 		break
