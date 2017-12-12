0dbcf9075e66ac34556fdaef89e27eadac41ce6e
Chetan Subhash Patil <chetan.subhash.patil@gmail.com>
2017-12-13 01:55:39 +0530

    test

--- shellproject/utilities/utility.sh
+++ shellproject/utilities/utility.sh
@@ -4,4 +4,4 @@
 rsync -avz --no-l --exclude "*.class" chetpati@adc00raj.us.oracle.com:/scratch/chetpati/chetan/work/rsync /cygdrive/d/chetan/work
 rsync -avz --no-l --exclude "*.class" "/cygdrive/d/chetan/work/rsync/chetpati_bip_main/xdo" "/cygdrive/d/chetan/work/bipublisher/chetpati_bip_main_backup"
 
-robocopy d:/chetan/work/rsync/chetpati_bip_main d:/chetan/work/bipublisher/chetpati_bip_main /E /MT:64
+robocopy d:/chetan/work/rsync/chetpati_bip_main d:/chetan/work/bipublisher/chetpati_bip_main /E /XD d:/chetan/work/rsync/chetpati_bip_main/xdo/bin /XF *.class .classpath .tomcatplugin .project  /MT:64
